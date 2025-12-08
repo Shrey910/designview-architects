5f03b95a916de029b200b0724dd7ccf308f92413
Shrey910 <shreymistry910@gmail.com>
2025-12-08 17:56:18 +0530

    tab update

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 import CustomCursor from '../components/CustomCursor';
 
 export const metadata = {
-  title: 'Luxury Interior Design Studio',
+  title: 'Designview Architects',
   description: 'Premium interior design services for discerning clients',
 };
 
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/dwlogo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=TASA+Explorer:wght@400..800&display=swap" rel="stylesheet" />
